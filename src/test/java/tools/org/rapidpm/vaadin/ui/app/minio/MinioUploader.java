b4de66ea54c83f274f6c8b07680e1b5bf288bed8
Sven Ruppert <sven.ruppert@gmail.com>
2018-03-07 15:02:16 +0100

    FileUploader as Tool

---
+++ src/test/java/tools/org/rapidpm/vaadin/ui/app/minio/MinioUploader.java
@@ -0,0 +1,84 @@
+package tools.org.rapidpm.vaadin.ui.app.minio;
+
+import org.rapidpm.frp.functions.CheckedFunction;
+import org.rapidpm.frp.model.Result;
+import org.rapidpm.vaadin.ui.server.imageservice.s3.Coordinates;
+
+import java.io.File;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+import static java.lang.Boolean.TRUE;
+import static java.nio.file.Files.newDirectoryStream;
+import static java.nio.file.Files.newInputStream;
+import static java.nio.file.StandardOpenOption.READ;
+import static org.rapidpm.vaadin.ui.app.DashboardComponent.DEFAULT_BUCKET_NAME;
+import static org.rapidpm.vaadin.ui.server.imageservice.s3.minio.MinioClientFunctions.*;
+
+/**
+ *
+ */
+public class MinioUploader {
+
+  private static final Consumer<String> logger = System.out::println;
+
+
+  //TODO put here your target infos
+  public static Supplier<Coordinates> access() {
+    return () -> new Coordinates("http://127.0.0.1:9001",
+                                 "minio",
+                                 "minio123"
+    );
+  }
+
+
+  public static CheckedFunction<String, DirectoryStream<Path>> dirStream() {
+    return (path) -> newDirectoryStream(new File(path).toPath());
+  }
+
+  public static CheckedFunction<Path, Blob> imageBlob() {
+    return (p) -> new Blob(
+        DEFAULT_BUCKET_NAME,
+        p.getFileName().toString(),
+        newInputStream(p, READ),
+        "image/jpg"
+    );
+  }
+
+  /**
+   * Example of nested exceptions, and how to deal with them
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    String path = "./_data/_nasa_pics/_0512px/";
+
+    client()
+        .apply(access().get())
+        .thenCombine(DEFAULT_BUCKET_NAME, bucket())
+        .ifFailed(logger)
+        .ifPresentOrElse(
+            minioClient -> dirStream().apply(path)
+                                      .ifFailed(logger)
+                                      .thenCombine(minioClient, (stream, c) -> {
+                                        for (final Path p : stream) {
+                                          imageBlob()
+                                              .apply(p)
+                                              .ifPresentOrElse(
+                                                  blob -> putObj()
+                                                      .apply(c, blob)
+                                                      .ifFailed(logger)
+                                                  ,
+                                                  logger
+                                              );
+                                        }
+                                        return Result.success(TRUE);
+                                      }),
+            logger
+        );
+
+
+  }
+}
