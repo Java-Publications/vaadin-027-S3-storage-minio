8bff6f36aede36fa25bc76091493e1e4307b08ab
Sven Ruppert <sven.ruppert@gmail.com>
2018-03-08 10:44:04 +0100

    core image functions

--- src/main/java/org/rapidpm/vaadin/ui/server/imageservice/ImageFunctions.java
+++ src/main/java/org/rapidpm/vaadin/ui/server/imageservice/ImageFunctions.java
@@ -1,5 +1,12 @@
 package org.rapidpm.vaadin.ui.server.imageservice;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -19,4 +26,40 @@ public interface ImageFunctions {
   static Supplier<String> nextImageName() {
     return () -> "nasa_pic_" + randomImageID().apply(100) + ".jpg";
   }
+
+  static Function<String, byte[]> failedImage() {
+    return (imageID) -> {
+      BufferedImage image = new BufferedImage(512, 512,
+                                              BufferedImage.TYPE_INT_RGB
+      );
+      Graphics2D drawable = image.createGraphics();
+      drawable.setStroke(new BasicStroke(5));
+      drawable.setColor(Color.WHITE);
+      drawable.fillRect(0, 0, 512, 512);
+      drawable.setColor(Color.BLACK);
+      drawable.drawOval(50, 50, 300, 300);
+
+      drawable.setFont(new Font("Montserrat", Font.PLAIN, 20));
+      drawable.drawString(imageID, 75, 216);
+      drawable.setColor(new Color(0, 165, 235));
+      try {
+        // Write the image to a buffer
+        ByteArrayOutputStream imagebuffer = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", imagebuffer);
+
+        // Return a stream from the buffer
+        return imagebuffer.toByteArray();
+      } catch (IOException e) {
+        return new byte[0];
+      }
+    };
+  }
+
+  static Function<String, InputStream> failedImageAsInputStream() {
+    return (imageID) -> failedImage()
+        .andThen(ByteArrayInputStream::new)
+        .apply(imageID);
+  }
+
+
 }
